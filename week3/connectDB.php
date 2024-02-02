0a810b9172be32ad0b9763e75e917b6958eb6aae
unknown <L5903@bangkhen.spu.ac.th>
2024-02-02 14:17:55 +0700

    [ADD]-add sql code

--- week3/connectDB.php
+++ week3/connectDB.php
@@ -17,16 +17,32 @@
             $sql = "select * from Customer where C_ADDRESS = '".$address."'";
             $result = sqlsrv_query($conn,$sql);
         }
-
         if(isset($_POST['cusBuyProduct'])){
-            $sql = "select * from Customer where C_ADDRESS = '".$address."'";
+            $sql = "select
+            c.C_ID,P_NAME
+            from CUSTOMER c, PRODUCT p, ORDERED o, ORDER_DETAIL od
+            where  c.C_ID = o.C_ID and o.ORDER_ID = od.ORDER_ID and od.P_ID = p.P_ID";
             $result = sqlsrv_query($conn,$sql);
+
+            echo ("<table border =1>");
+            echo ("<tr>".
+                    "<td>รหัสลูกค้า</td>".
+                    "<td>สินค้า</td>".
+                "</tr>");
+            while($row = sqlsrv_fetch_array($result, SQLSRV_FETCH_ASSOC)){
+                echo ("<tr>".
+                    "<td>".$row['C_ID']."</td>".
+                    "<td>".$row['P_NAME']."</td>".
+                "</tr>");
+
+            }
+            echo ("</table>");
         }
 
         if($result === false){
             die(print_r(sqlsrv_errors(),true));
         }else{
-            echo ("<table>");
+            echo ("<table border =1>");
             echo ("<tr>".
                     "<td>รหัสลูกค้า</td>".
                     "<td>ชื่อ</td>".
